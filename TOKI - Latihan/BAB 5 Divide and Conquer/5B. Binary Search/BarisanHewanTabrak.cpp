0c5122eb7e97afb19e9daea75c80f09a65776611
hierony94 <hierony.manurung@gdplabs.id>
2016-04-18 08:53:47 +0700

    Fix some answer in TOKI solutionset

--- TOKI - Latihan/BAB 5 Divide and Conquer/5B. Binary Search/BarisanHewanTabrak.cpp
+++ TOKI - Latihan/BAB 5 Divide and Conquer/5B. Binary Search/BarisanHewanTabrak.cpp
@@ -1,20 +1,20 @@
 // Template by Hierony
- 
+
 #include <cstdio>
 #include <cmath>
 #include <cstring>
 #include <cstdlib>
 #include <iostream>
- 
+
 #include <string>
 #include <algorithm>
 #include <vector>
 #include <map>
 #include <stack>
 #include <queue>
- 
+
 using namespace std;
- 
+
 typedef long long LL;
 typedef unsigned long long ULL;
 typedef pair<int,int> pii;
@@ -28,42 +28,42 @@ typedef vector<LL> vl;
 typedef vector<vl> vvl;
 typedef vector<string> vstr;
 typedef vector<char> vc;
- 
+
 double PI = acos(-1);
- 
+
 #define REP(i,n) for((i) = 0; (i) < (int)(n); (i)++)
-#define READ_INT(data) scanf("%d",&data); 
+#define READ_INT(data) scanf("%d",&data);
 #define RESET(c,v) memset(c, v, sizeof(c))
 #define MAX(a,b) a = max(a,b)
 #define MIN(a,b) a = min(a,b)
 #define pb push_back
 #define mp make_pair
- 
+
 inline string IntToString(int a){
     char x[100];
     sprintf(x,"%d",a); string s = x;
     return s;
 }
- 
+
 inline int StringToInt(string a){
     char x[100]; int res;
     strcpy(x,a.c_str()); sscanf(x,"%d",&res);
     return res;
 }
- 
+
 inline string GetString(void){
     char x[1000005];
     scanf("%s",x); string s = x;
     return s;
 }
- 
+
 vector <ULL> data2;
-int n,Q; 
-int a,b; 
+int n,Q;
+int a,b;
 ULL temp;
- 
-int Bin(int kanan, int soal){
-		int kiri = 0,pivot; 
+
+int Binser(int kanan, int soal){
+		int kiri = 0,pivot;
         while (kiri <= kanan){
             pivot =  (kiri+kanan)/2;
             //printf("Pivot : %d\n",pivot);
@@ -71,22 +71,22 @@ int Bin(int kanan, int soal){
                 return pivot;
             }
             // Jika tidak ditemukan
-            else{        
+            else{
 				if(soal > data2[pivot]) kiri = pivot+1;
                 else if(soal < data2[pivot]) kanan = pivot-1;
             }
         }
-        if(soal < data2[pivot]) return pivot;	
+        if(soal < data2[pivot]) return pivot;
         else return pivot + 1;
-        
-}	 
- 
+
+}
+
 int main()
- {	
+ {
     ULL total = 0;
     int nAsk;
  	scanf("%d",&n);
- 	
+
  	// Menggenerate Data
 	REP(a,n)
  	{
@@ -94,13 +94,12 @@ int main()
  		total += temp;
  		data2.pb(total);
  	}
- 	
  	scanf("%d",&Q);
  	REP(a,Q)
  	{
- 		scanf("%d",&nAsk);		
-		printf("%d\n",Bin(n,nAsk) + 1);
+ 		scanf("%d",&nAsk);
+		printf("%d\n",Binser(n,nAsk) + 1);
  	}
- 	
+
 	return 0;
- } 
+ }
